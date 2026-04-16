635adf298d72ba23ffef79b6dc8da1be6425580e
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-04-16 16:08:39 +0300

    get posts data from the server

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here!
+async function getPostsData() {
+    try {const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    const data =  await response.json()
+    displayPosts(data)
+    return data
+    
+}catch(error){
+    console.log(error)
+}
+}
+
+function displayPosts(posts){
+   
+const postList = document.getElementById('post-list')
+ postList.innerHTML=""
+posts.forEach((post)=>{
+const li = document.createElement("li")
+const h1 = document.createElement("h1")
+const p = document.createElement("p")
+h1.textContent = post.title
+p.textContent= post.body
+
+li.appendChild(h1)
+li.appendChild(p)
+postList.appendChild(li)
+
+})
+
+
+}
+getPostsData()
